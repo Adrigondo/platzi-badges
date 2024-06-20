d7195994f3b6d2fdba4ddb929207415e9a27fbed
Adora <im.stariluz@gmail.com>
2024-06-20 14:19:37 -0600

    Try of deploy

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  console.log(process.env.VITE_REPO_NAME);
+  console.log(process.env);
   return {
     plugins: [
       {

85ba7510f39583c4e756c681c2d2ef3951db69a5
Adora <im.stariluz@gmail.com>
2024-06-19 23:55:32 -0600

    Fixing docker image, and allowing app to run in production

--- src/api.js
+++ src/api.js
@@ -1,9 +1,9 @@
-const BASE_URL = 'http://localhost:3001';
+const BASE_URL = process.env.BASE_URL;
 
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 const randomNumber = (min = 0, max = 1) =>
   Math.floor(Math.random() * (max - min + 1)) + min;
-const simulateNetworkLatency = (min = 30, max = 1500) =>
+const simulateNetworkLatency = (min = 30, max = 800) =>
   delay(randomNumber(min, max));
 
 async function callApi(endpoint, options = {}) {

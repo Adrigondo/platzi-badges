d0e3ad6346b9895dd10f1ad89c72ae43dc02105b
Adora <im.stariluz@gmail.com>
2024-06-20 14:39:43 -0600

    Testing deploy

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv('', process.cwd(), '');
+  const env = loadEnv(mode, 'environment');
   console.log(mode, env);
   return {
     plugins: [
